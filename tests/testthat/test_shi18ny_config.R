530581d3d822930312fe57046a651eff87bb6597
Juan Pablo Marin Diaz <jpmarindiaz@gmail.com>
2020-04-24 18:57:01 -0500

    added test comment

--- tests/testthat/test_shi18ny_config.R
+++ tests/testthat/test_shi18ny_config.R
@@ -42,12 +42,13 @@ test_that("i18n Config",{
   config <- i18nConfig(opts)
   expect_error(i18nLoad(opts),"Requested languages not in locale folder")
 
-  i18n <- list(
-    defaultLang = "en",
-    availableLangs = c("es","en")
-  )
-  i18n <- i18nLoad(i18n)
-  config <- i18n$.config
+  # TODO need to add more tests here
+  # i18n <- list(
+  #   defaultLang = "en",
+  #   availableLangs = c("es","en")
+  # )
+  # i18n <- i18nLoad(i18n)
+  # config <- i18n$.config
 
 
 })
