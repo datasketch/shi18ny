318f304e71e5058a2eb9568734582440061c9d44
Juan Pablo Marin Diaz <jpmarindiaz@gmail.com>
2020-04-25 10:32:02 -0500

    custom translatons override defautls

--- tests/testthat/test_translations.R
+++ tests/testthat/test_translations.R
@@ -24,98 +24,15 @@ test_that("i18n Config",{
   str <- c("download", "upload")
   lang <- "es"
   expect_equal(i_(str, lang), c("descargar", "subir"))
+
+  # Override default with custom translations
+  expect_equal(i_("hello", "es"), "hola")
+  expect_equal(i_("hello", "es", i18n), "Hola!!!")
+
+
 })
 
 
-test_that("list translations work",{
 
-  localeDir <- system.file("examples/locale",package = "shi18ny")
-  opts <- list(
-    localeDir = localeDir,
-    defaultLang = "es",
-    fallbacks = list("es" = "en")
-  )
-  config <- i18nConfig(opts)
-  i18n <- i18nLoad(opts)
-
-  l <- list(label = "hello", numbers = "abort",
-            custom_text = "myslang.hi",
-            undefined = "undefined.hi",
-            x = "abort")
-  l_es <- i_list(l, lang = "es", i18n = i18n,
-                 keys = c("label","numbers", "custom_text", "undefined"))
-  expect_equal(names(l), names(l_es))
-  expect_equal(l_es$label, "hola")
-  expect_equal(l_es$numbers, "Cancelar")
-  expect_equal(l_es$custom_text, "Quiubo")
-  expect_equal(l_es$undefined, "undefined.hi")
-
-  # Check it works with nested lists
-  l <- list(
-    label = "hello",
-    numbers = "myslang.hi",
-    info = list(title = "hello world", information = "information",
-         more = list(
-           label = "abort",
-           more2 = "more2"
-         ))
-  )
-
-  xl <- i_list(l, lang = "es", i18n = i18n, keys = c("label","numbers","title"))
-  expect_equal(xl$label, "hola")
-  expect_equal(xl$numbers, "Quiubo")
-  expect_equal(xl$info$more$label, "Cancelar")
-  expect_equal(xl$info$more$more2, l$info$more$more2)
-
-  xl2 <- i_(l, lang = "es", i18n = i18n, keys = c("label","numbers","title"))
-  expect_equal(xl2$label, "hola")
-  expect_equal(xl2$numbers, "Quiubo")
-  expect_equal(xl2$info$more$label, "Cancelar")
-  expect_equal(xl2$info$more$more2, l$info$more$more2)
-
-  expect_equal(xl, xl2)
-
-  # Check it works with chr and non-chr lists
-
-  l <- list(
-    label = "hello",
-    numbers = 1:3,
-    info = list(title = "hello world", information = TRUE,
-                more = list(
-                  label2 = "abort",
-                  more2 = ggplot2::qplot()
-                ))
-  )
-  xl <- i_list(l, "es", keys = c("label","label2"))
-  expect_equal(xl$label, "hola")
-  expect_equal(xl$numbers, 1:3)
-  expect_equal(xl$info$more$label2, "Cancelar")
-  expect_equal(xl$info$more$more2, l$info$more$more2)
-
-  # Check keys work
-  # First level
-  l <- list(label = "hello", information = "information")
-  xl <- i_list(l, "es", keys = c("information"))
-  expect_equal(xl$label, "hello")
-  expect_equal(xl$information, "información")
-  xl <- i_list(l, "es", keys = c("label"))
-  expect_equal(xl$label, "hola")
-  expect_equal(xl$information, "information")
-
-  # More levels
-  l <- list(label = "hello", information = "information",
-            more = list(label = "hello", information = "information"))
-  xl <- i_list(l, "es", keys = c("information"))
-  expect_equal(xl$label, "hello")
-  expect_equal(xl$information, "información")
-  expect_equal(xl$more$label, "hello")
-  expect_equal(xl$more$information, "información")
-  xl <- i_list(l, "es", keys = c("label"))
-  expect_equal(xl$label, "hola")
-  expect_equal(xl$information, "information")
-  expect_equal(xl$more$label, "hola")
-  expect_equal(xl$more$information, "information")
-
-})
 
 
