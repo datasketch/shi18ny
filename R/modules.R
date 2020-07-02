73248e8223ab80712bfeada0e694f90aabdabd33
Lena Mangold <lena@mangold.org>
2020-07-02 12:38:24 -0500

    add doc for langSelector and langSelectorInput

--- R/modules.R
+++ R/modules.R
@@ -1,3 +1,14 @@
+#' Language selector input
+#'
+#' This function needs to be included in the UI to create the
+#' language switcher in the Shiny app.
+#'
+#' @param id The id that will be used to access the value.
+#' @param position String indicating position of language switcher; options
+#'   are "right" and "fixed"; default = "right".
+#' @param width Width of language switcher container; default = 80.
+
+
 #' @export
 langSelectorInput <- function(id,
                               position = "right",
@@ -24,6 +35,70 @@ langSelectorInput <- function(id,
     )
   )
 }
+
+#' Language selector
+#'
+#' This Shiny module gets language that's currently selected in the language
+#' selector. It needs to be used in the
+#' Server to save the currently active language in a reactive. This reactive
+#' can then be passed to the `i_` (TODO link to function documentation!)
+#' function to translate the text.
+#'
+#' @param i18n List of language configurations;
+#'   Options that can be set are:
+#'
+#'   `defaultLang` Default language used in Shiny app; default = "en"
+#'
+#'   `availableLangs` Language that can be chosen for translation in Shiny app;
+#'   there are currently 15 available languages (see **Available languages**);
+#'   defaults to all available languages
+#'
+#'   `localeDir` Directory to `yaml` files which contain custom keyword
+#'   translations; default = "locale"
+#'
+#'   `fallbacks` List of fallback languages if translation for a word is not
+#'   found in desired language; defaults to **Default fallbacks**
+#'
+#'   `queryParameter` String to define query parameter if language to be set
+#'   through URL; default = "lang"
+#'
+#' @param showSelector Boolean to specify if language selector should be
+#'   displayed or not. Even if selector is not displayed, language can always
+#'   be set by query parameter; default = "lang"
+#'
+#' @return String of language code of currently selected languages.
+#'
+#' @section Available languages: There are currently 15 languages available for
+#'   translation:
+#'
+#'  | code  | language            |
+#'  | ----- | ------------------- |
+#'  | ar    | Arabic              |
+#'  | ca    | Catalan             |
+#'  | da    | Danish              |
+#'  | de    | German              |
+#'  | en    | English             |
+#'  | es    | Spanish             |
+#'  | fr    | French              |
+#'  | he    | Hebrew              |
+#'  | hi    | Hindi               |
+#'  | it    | Italian             |
+#'  | pt    | Portuguese          |
+#'  | pt_BR | Portuguese (Brazil) |
+#'  | ru    | Russian             |
+#'  | sv    | Swedish             |
+#'  | zh_CN | Chinese             |
+#'
+#' @section Default fallbacks: If no fallback languages are specified,
+#'   translations automatically fall back onto the following languages.
+#'
+#'  | original language  | fallback language |
+#'  | ------------------ | ----------------- |
+#'  | es                 | pt                |
+#'  | pt                 | es                |
+#'  | fr                 | pt                |
+#'  | de                 | nl                |
+#'  | nl                 | de                |
 
 #' @export
 langSelector <- function(input, output, session,
