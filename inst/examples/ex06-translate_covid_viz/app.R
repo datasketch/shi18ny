1ea634f9b657c2b0fa3621f385f71f9e799ee4db
Lena Mangold <lena@mangold.org>
2020-07-09 10:26:59 -0500

    update example for new data

--- inst/examples/ex06-translate_covid_viz/app.R
+++ inst/examples/ex06-translate_covid_viz/app.R
@@ -5,8 +5,13 @@ library(leaflet)
 library(shi18ny)
 library(lfltmagic)
 
-data <- read.csv("tidy-csse-langs.csv") %>%
-  filter(date == "2020-04-12") %>%
+load_data <- read.csv("tidy-csse-langs.csv")
+
+max_date <- max(load_data$date)
+
+data <- load_data %>%
+  group_by(country) %>%
+  filter(date == max_date) %>%
   select(iso_alpha3, confirmed, country, en, de, es)
 
 ui <- fluidPage(
@@ -16,31 +21,16 @@ ui <- fluidPage(
   h3(ui_("internationalize")),
   hr(),
   fluidRow(
-
     column(6,
            leafletOutput(outputId = "world_map")
     )),
   hr(),
   h5(ui_("annotate")),
   hr(),
-  h6(ui_("status")),
+  uiOutput("datestamp")
 )
 
 server <- function(input, output) {
-
-  output$world_map <- renderLeaflet({
-    country = i_("country", lang())
-    cases = i_("cases", lang())
-
-    lflt_choropleth_Gcd(data,
-                        map_tiles = "CartoDB",
-                        tooltip = paste0("<b>",country,":</b> {",lang(),"} </br> <b>",cases,":</b> {confirmed}"),
-                        palette_colors = c("#99f2c8", "#1f4037"),
-                        branding_include = TRUE,
-                        title = i_("title", lang()),
-                        subtitle = i_("subtitle", lang())
-                        )
-  })
 
   i18n <- list(
     defaultLang = "en",
@@ -52,6 +42,25 @@ server <- function(input, output) {
   observeEvent(lang(),{
     uiLangUpdate(input$shi18ny_ui_classes, lang())
   })
+
+  output$world_map <- renderLeaflet({
+
+    country = i_("country", lang())
+    cases = i_("cases", lang())
+
+    lflt_choropleth_Gcd(data,
+                        map_tiles = "CartoDB",
+                        tooltip = paste0("<b>",country,":</b> {",lang(),"} </br> <b>",cases,":</b> {confirmed}"),
+                        palette_colors = c("#99f2c8", "#1f4037"),
+                        branding_include = TRUE,
+                        title = i_("title", lang()),
+                        subtitle = i_("subtitle", lang())
+                        )
+    })
+
+  output$datestamp <- renderUI({
+    h6(paste0(i_("status",lang()), ": ", max_date))
+    })
 
 }
 
