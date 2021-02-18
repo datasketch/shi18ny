284c1923e1c24f9ad32f98b76bdc60e123998705
Lena Mangold <lena@mangold.org>
2021-02-17 19:48:11 -0500

    remove update input fct

--- R/modules.R
+++ R/modules.R
@@ -113,40 +113,50 @@ langSelector <- function(input, output, session,
                          i18n = NULL, showSelector = TRUE){
   i18n <- i18nLoad(i18n)
   config <- i18n$.config
+
   queryLang <- reactive({
     query <- parseQueryString(session$clientData$url_search)
     query[[config$queryParameter]]
   })
+
   # observe({
-  initLocale <- reactive({
-    selected <- queryLang() %||% config$defaultLang
-    message("selected2 ", selected," config ", config$defaultLang)
-    message(showSelector)
-    if(showSelector){
-      #   message(showSelector)
-      #   return(queryLang())
-      # }else{
-      shinyjs::show("langContainer")
-    }
-    message("config_av_langs",config$availableLangs)
-    updateSelectLangInput(session, 'langInner',
-                          langs = config$availableLangs,
-                          selected = selected)
-    # return(reactive(selected))
-    #selected <- queryLang()
-    # if(is.null(selected)) return(config$defaultLang)
-    # if(!showSelector) return(selected)
-    message("selected3", selected, " config ", config$defaultLang)
-    selected
-  })
+  # initLocale <- reactive({
+  #   selected <- queryLang() %||% config$defaultLang
+  #   message("selected2 ", selected," config ", config$defaultLang)
+  #   message(showSelector)
+  #   if(showSelector){
+  #     #   message(showSelector)
+  #     #   return(queryLang())
+  #     # }else{
+  #     shinyjs::show("langContainer")
+  #   }
+  #   message("config_av_langs",config$availableLangs)
+  #   updateSelectLangInput(session, 'langInner',
+  #                         langs = config$availableLangs,
+  #                         selected = selected)
+  #   # return(reactive(selected))
+  #   #selected <- queryLang()
+  #   # if(is.null(selected)) return(config$defaultLang)
+  #   # if(!showSelector) return(selected)
+  #   message("selected3", selected, " config ", config$defaultLang)
+  #   selected
+  # })
+#
+#   observe({
+#     # browser()
+#     updateSelectLangInput(session, 'langInner',
+#                           langs = config$availableLangs,
+#                           selected = queryLang() %||% config$defaultLang)
+#   })
 
   currentLocale <- reactive({
-    initLocale()
-    message("initLocale: ", initLocale())
-    message("input3 ", input$langInner, " is null ",is.null(input$langInner))
+    # initLocale()
+    # message("initLocale: ", initLocale())
+    message("input3 ", queryLang(), " is null ",is.null(queryLang()))
     # if(is.null(input$langInner))
     #   return(initLocale())
-    input$langInner
+    queryLang()
+    # browser()
   })
   currentLocale
 }
